a51063689a49e79d14b493fe183bdf0b8c4ea543
Jimmysb <jbailey@jimmysoftllc.com>
2019-12-05 17:00:32 -0500

    tutorial updated

--- js/api.js
+++ js/api.js
@@ -1,10 +1,44 @@
 const URL_Address =
-  'https://ig4y9n2273.execute-api.us-east-1.amazonaws.com/default/testFunction';
+  'https://ytmzpfsdrd.execute-api.us-east-1.amazonaws.com/default/testFunction';
 
-function getTable() {
+function scan() {
   console.log('Getting data from AWS dynamoDB');
   fetch(URL_Address + '/?TableName=test', {
     method: 'get',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res =>
+      res.json().then(data => ({
+        status: res.status,
+        body: data,
+      }))
+    )
+    .then(obj => {
+      if (obj.status === 200) {
+        console.log(obj.body);
+      } else {
+        console.log('Error', obj.body.error);
+      }
+    });
+}
+
+function putItem() {
+  console.log('Putting data to AWS dynamoDB');
+  var randomPrimaryKey = Date.now();
+  fetch(URL_Address, {
+    method: 'post',
+      body: JSON.stringify({
+            TableName: 'test',
+            Item: {
+              myPrimaryKey: randomPrimaryKey,
+              mySortKey: 'any sort key',
+              myData: 'added some new data',
+            },
+            ReturnConsumedCapacity: 'TOTAL',
+    }),
     headers: {
       Accept: 'application/json, text/plain, */*',
       'Content-Type': 'application/json',
---
+++ js/lambda.js
@@ -0,0 +1,55 @@
+console.log('Loading function');
+
+const doc = require('dynamodb-doc');
+
+const dynamo = new doc.DynamoDB();
+
+
+/**
+ * Demonstrates a simple HTTP endpoint using API Gateway. You have full
+ * access to the request and response payload, including headers and
+ * status code.
+ *
+ * To scan a DynamoDB table, make a GET request with the TableName as a
+ * query string parameter. To put, update, or delete an item, make a POST,
+ * PUT, or DELETE request respectively, passing in the payload to the
+ * DynamoDB API as a JSON body.
+ */
+exports.handler = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',           
+            'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+            'Access-Control-Allow-Headers': 'Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+        },
+    });
+
+    switch (event.httpMethod) {
+        case 'OPTIONS':
+             const response = {
+                 statusCode: 200,
+                 body: 'CORS check passed safe to proceed',
+             };
+             done(null,response);
+             break;
+        case 'DELETE':
+            dynamo.deleteItem(JSON.parse(event.body), done);
+            break;
+        case 'GET':
+            dynamo.scan({ TableName: event.queryStringParameters.TableName }, done);
+            break;
+        case 'POST':
+            dynamo.putItem(JSON.parse(event.body), done);
+            break;
+        case 'PUT':
+            dynamo.updateItem(JSON.parse(event.body), done);
+            break;
+        default:
+            done(new Error(`Unsupported method "${event.httpMethod}"`));
+    }
+};
